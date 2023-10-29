03b7ba2b5e2da5954eddcf3404e3e3f347e82523
SammCro <ilkay@beevision.ai>
2023-10-29 16:24:42 +0300

    enhanced switch statement is added.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import javax.annotation.processing.SupportedSourceVersion;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+
+    // enhanced switch statement with expressions
+    public static String enhancedSwitch(StringBuilder day){
+        return switch (day.toString()){
+            case "first" -> "Monday"; // {yield "Monday"}
+            case "second" -> "Tuesday";
+            default -> {
+                String badDay = day + " bad input";
+                yield badDay;
+            }
+        };
     }
 }
